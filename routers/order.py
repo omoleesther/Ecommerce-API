a859f2ae80cf8692912fcda2139a36e048704ede
omole <estheromole08@gmail.com>
2026-05-05 16:48:46 +0100

    implemented order status

--- routers/order.py
+++ routers/order.py
@@ -1,126 +1,223 @@
-from fastapi import APIRouter, Depends
-from schema.order import CreateOrder, UpdateOrder
+from fastapi import APIRouter, Depends, status
 from utils.auth import get_current_active_user
 from fastapi import HTTPException
 import models
-from datetime import datetime
 from utils.getdb import db_dependency
 router = APIRouter()
 
 
-@router.post("/create-order")
-def create_order(order: CreateOrder, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+@router.get("/orders/pending")
+def all_orders(db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
     if current_user.role != "customer":
         raise HTTPException(
             status_code=401, detail="Unauthorized"
         )
 
-    product = db.query(models.Product).filter(
-        models.Product.id == order.product_id,
-        models.Product.is_active == True).first()
-    if product is None:
-        raise HTTPException(status_code=404, detail="Product does not exixts")
+    all_order = db.query(models.Order).filter(
+        models.Order.customer_id == current_user.id, models.Order.status == "pending").first()
 
-    if order.quantity <= 0:
+    if not all_order:
         raise HTTPException(
-            status_code=400, detail="Quantity must be greater than 0")
-
-    if order.quantity > product.stock:
-        raise HTTPException(
-            status_code=400, detail="Quantity is more than the available stock")
-
-    total_amount = order.quantity * product.price
-
-    orders = models.Order(
-        product_id=product.id,
-        product_name=product.name,
-        customer_id=current_user.id,
-        total_amount=total_amount,
-        status="pending",
-        created_at=datetime.utcnow()
-    )
-    db.add(orders)
-    db.commit()
-    db.refresh(orders)
-
-    order_item = models.OrderItem(
-        order_id=orders.id,
-        product_id=product.id,
-        product_name=product.name,
-        quantity=order.quantity,
-        price_at_time=product.price
-    )
-    db.add(order_item)
-
-    product.stock -= order.quantity
-
-    db.commit()
-    return {
-        "message": "Order Created",
-        "Order_id": {orders.id},
-        "status": {orders.status},
-        "Product name": {orders.product_name},
-        "total_amount": {orders.total_amount}
-
-    }
-
-
-@router.get("/get-all-orders-admin")
-def all_orders(db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
-    if current_user.role != "admin":
-        raise HTTPException(
-            status_code=401, detail="Unauthorized"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No order pending"
         )
-
-    all_order = db.query(models.OrderItem).join(models.Product, models.Product.id == models.OrderItem.product_id).filter(
-        models.Product.owner_id == current_user.id).all()
-
     return all_order
 
 
-@router.get("/my-order")
-def my_order(db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
-    if current_user.role != "customer":
+@router.get("/all_order/admin")
+def orders(db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+    if current_user.role != "admin":
         raise HTTPException(
-            status_code=403, detail="Unauthorized"
+            status_code=401, detail="Unauthorized"
+        )
+    orders = db.query(models.Order).all()
+    if not orders:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No orders"
         )
 
-    item = db.query(models.OrderItem).join(models.Order, models.Order.id ==
-                                           models.OrderItem.order_id).filter(models.Order.customer_id == current_user.id).all()
-    if not item:
-        raise HTTPException(status_code=403, detail="Cart Empty")
-    return item
+    return orders
 
 
-"""
-@router.put("/update-order")
-def update_order(order_id: int, change_order: UpdateOrder, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+@router.get("/all_order")
+def orders(db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+    if current_user.role != "customer":
+        raise HTTPException(
+            status_code=401, detail="Unauthorized"
+        )
+    orders = db.query(models.Order).filter(
+        models.Order.customer_id == current_user.id).all()
+    if not orders:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No orders"
+        )
+
+    return orders
+
+
+@router.get("/orders/{order_id}")
+def orders(order_id: int, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+    if current_user.role != "customer":
+        raise HTTPException(
+            status_code=401, detail="Unauthorized"
+        )
+
+    order = db.query(models.Order).filter(
+        models.Order.id == order_id
+    ).first()
+
+    if not order:
+        raise HTTPException(
+            status_code=404,
+            detail="Order not found"
+        )
+    items = db.query(models.OrderItem).filter(
+        models.OrderItem.order_id == order_id
+    ).all()
+
+    if not items:
+        raise HTTPException(status_code=404, detail="Cart Empty")
+    return {
+        "order_id": order.id,
+        "status": order.status,
+        "items": items
+    }
+
+
+@router.get("/orders/{order_id}")
+def orders(order_id: int, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+
+    order = db.query(models.Order).filter(
+        models.Order.id == order_id
+    ).first()
+
+    if not order:
+        raise HTTPException(
+            status_code=404,
+            detail="Order not found"
+        )
+    if current_user.role == "customer" and order.customer_id != current_user:
+        raise HTTPException(
+            status_code=401, detail="Unauthorized"
+        )
+
+    items = db.query(models.OrderItem).filter(
+        models.OrderItem.order_id == order_id
+    ).all()
+
+    if not items:
+        raise HTTPException(status_code=404, detail="Cart Empty")
+    return {
+        "order_id": order.id,
+        "status": order.status,
+        "items": items
+    }
+
+
+@router.patch("/orders/{order_id}/shipped")
+def shipped_orders(order_id: int, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
     if current_user.role != "admin":
-        raise HTTPException(status_code=403, details="Unauthorized")
+        raise HTTPException(
+            status_code=401, detail="Unauthorized"
+        )
+    all_order = db.query(models.Order).filter(
+        models.Order.id == order_id,
+        models.Order.status == "pending").first()
 
-    product = db.query(models.Order).filter(
+    if not all_order:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No order pending"
+        )
+
+    all_order.status = "shipped"
+
+    db.commit()
+    db.refresh(all_order)
+    return {
+        "message":  "Order updated successfully",
+        "order_id": all_order.id,
+        "status": all_order.status
+    }
+
+
+@router.patch("/orders/{order_id}/out_for_delivery")
+def out_for_delivery(order_id: int, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=401, detail="Unauthorized"
+        )
+    all_order = db.query(models.Order).filter(
+        models.Order.id == order_id,
+        models.Order.status == "shipped").first()
+
+    if not all_order:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No order shipped"
+        )
+
+    all_order.status = "out_for_delivery"
+
+    db.commit()
+    db.refresh(all_order)
+    return {
+        "message":  "Order updated successfully",
+        "order_id": all_order.id,
+        "status": all_order.status
+    }
+
+
+@router.patch("/orders/{order_id}/delivered")
+def delivered(order_id: int, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=401, detail="Unauthorized"
+        )
+    all_order = db.query(models.Order).filter(
+        models.Order.id == order_id,
+        models.Order.status == "out_for_delivery").first()
+
+    if not all_order:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="No order out for delivery"
+        )
+
+    all_order.status = "delivered"
+
+    db.commit()
+    db.refresh(all_order)
+    return {
+        "message":  "Order updated successfully",
+        "order_id": all_order.id,
+        "status": all_order.status
+    }
+
+
+@router.patch("/orders/{order_id}/cancel")
+def cancel_order(order_id: int, db: db_dependency, current_user: models.User = Depends(get_current_active_user)):
+    if current_user.role != "customer":
+        raise HTTPException(
+            status_code=401, detail="Unauthorized"
+        )
+
+    all_order = db.query(models.Order).filter(
+        models.Order.customer_id == current_user.id,
         models.Order.id == order_id).first()
 
-
-
-   if product.
-
-    if db_product is None:
+    if not all_order:
         raise HTTPException(
-            status_code=404, detail="Product does not exixts")
+            status_code=400, detail="No order found"
+        )
 
-    if db_product.owner_id != current_user.id:
-        raise HTTPException(status_code=401, detail="Not your product")
+    if all_order.status != "pending":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Order can't be cancelled"
+        )
 
-    if product.name is not None:
-        db_product.name = product.name
+    all_order.status = "cancelled"
 
-    if product.description is not None:
-        db_product.description = product.description
-
-    if product.price is not None:
-        db_product.price = product.price
-
-    if product.stock is not None:
-        db_product.stock = product.stock
-"""
+    db.commit()
+    db.refresh(all_order)
+    return {
+        "message": "Order Cancelled",
+        "order_id": all_order.id,
+        "status": all_order.status
+    }
