d91471162d737f7a9d334f77a1d4b207f7980a20
omole <estheromole08@gmail.com>
2026-05-05 13:56:48 +0100

    refactored auth

--- utils/auth.py
+++ utils/auth.py
@@ -9,15 +9,16 @@ from typing import Optional, Literal
 from utils.authtoken import TokenData
 import models
 from datetime import datetime, timedelta
-
+from config import settings
 
 import resend
 from dotenv import load_dotenv
 load_dotenv()
-if not os.environ["RESEND_API_KEY"]:
+# os.environ["RESEND_API_KEY"]
+if not settings.RESEND_API_KEY:
     raise EnvironmentError("RESEND_API_KEY is missing")
 
-resend.api_key = os.getenv("RESEND_API_KEY")
+resend.api_key = settings.RESEND_API_KEY
 # resend = Resend(api_key=resend.api_key)
 
 
@@ -25,7 +26,7 @@ def send_email(to_email: str, confirmation_link: str):
     try:
         resend.Emails.send(
             {
-                "from": "estheromole08@gmail.com",
+                "from": "Resend <onboarding@resend.dev>",
                 "to": [to_email],
                 "subject": "Confirm your email",
                 "text": f"Click this link to confirm your account: {confirmation_link}"
@@ -39,14 +40,14 @@ def send_email(to_email: str, confirmation_link: str):
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = settings.SECRET_KEY
 # print("SECRET_KEY:", SECRET_KEY)
 ALGORITHM = "HS256"
 
 
 def create_credentials_exception(detail: str) -> HTTPException:
-    return HTTPException(
+    raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail=detail,
         headers={"WWW-Authenticate": "Bearer"}
@@ -61,44 +62,26 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def access_token_expire_minutes() -> int:
-    return 15
+def create_access_token(email: str, expires_delta: Optional[timedelta] = None):
+    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=15))
+    to_encode = {"sub": email, "exp": expire, "type": "access"}
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def confirm_token_expire_minutes() -> int:
-    return 1440
+def create_confirmation_token(email: str, expires_delta: Optional[timedelta] = None):
+    expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=1440))
+    to_encode = {"sub": email, "exp": expire, "type": "confirmation"}
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=access_token_expire_minutes)
-    to_encode.update({"sub": "email", "exp": expire, "type": "access"})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def create_confirmation_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=confirm_token_expire_minutes)
-    to_encode.update({"sub": "email", "exp": expire, "type": "confirmation"})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def authenticate_user(email: str, password: str):
-    user = get_current_user(email)
+def authenticate_user(email: str, password: str, db: Session):
+    user = get_user_by_email(email, db)
     if not user:
         create_credentials_exception("Invalid email or password")
     if not verify_password(password, user.password_hash):
         create_credentials_exception("Invalid email or password")
-    if not user.confirmed:
-        raise create_credentials_exception("User has not confirmed email")
+    # if not user.confirmed:
+      #  raise create_credentials_exception("User has not confirmed email")
     return user
 
 
@@ -109,19 +92,23 @@ def verify_token(token: str) -> TokenData:
         if email is None:
             create_credentials_exception("Token is missing 'sub' filed")
 
-        type = payload.get("type")
-        if type is None or type != "access":
+        token_type = payload.get("type")
+        if token_type is None or token_type != "access":
             raise create_credentials_exception(
-                "Token has incorrect type, expected'{type} ")
-        # return TokenData(email=email)
+                f"Token has incorrect type, expected'{token_type}' ")
+        return TokenData(email=email)
     except ExpiredSignatureError as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Token Expired",
             headers={"WWW-Authenticate": "Bearer"}
         )from e
-    except jwt.PyJWTError:
+    except jwt.JWTError:
         raise create_credentials_exception("Invalid Token ")
+
+
+def get_user_by_email(email: str, db: Session):
+    return db.query(models.User).filter(models.User.email == email).first()
 
 
 def get_subject_for_token_type(token: str, type: Literal["access", "confirmation"]) -> str:
@@ -129,18 +116,18 @@ def get_subject_for_token_type(token: str, type: Literal["access", "confirmation
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
     except ExpiredSignatureError as e:
-        raise create_access_token("Token Expired") from e
-    except jwt.PyJWTError:
+        raise create_credentials_exception("Token Expired") from e
+    except jwt.JWTError:
         raise create_credentials_exception("Invalid Token ")
 
     email: str = payload.get("sub")
     if email is None:
-        raise create_credentials_exception("Token is missing 'sub' filed")
+        raise create_credentials_exception("Token is missing 'sub' failed")
 
     token_type = payload.get("type")
     if token_type is None or token_type != type:
         raise create_credentials_exception(
-            "Token has incorrect type, expected'{type} ")
+            f"Token has incorrect type, expected'{token_type}' ")
 
     return email
 
